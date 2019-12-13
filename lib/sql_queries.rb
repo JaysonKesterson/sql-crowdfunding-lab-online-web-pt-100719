24f6e9cdd5a84f0f05df3b6ab4b38cf4e09b9288
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-13 02:08:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,8 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   "SELECT projects.title,SUM(pledges.amount)
    FROM projects
    JOIN pledges
-   ORDER_BY(title) ASC;"
+   ON projects.id = pledges.project_id
+   Group_by projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
