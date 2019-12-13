bd7f7eb21a681c16715e5e2c326ed929109631f5
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-13 01:57:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,8 +8,8 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT title,amount 
-  FROM projects 
-  ORDER_BY(title)"
+   FROM projects 
+   ORDER_BY(title)"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
